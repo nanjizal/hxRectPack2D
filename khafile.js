987d502d005673f34a8d275335234f7f168a0e0f
nanjizal <nanjizal@users.noreply.github.com>
2018-11-17 15:26:06 +0000

    Delete khafile.js

--- khafile.js
+++
@@ -1,9 +0,0 @@
-let project = new Project('TexturePacker Test');
-project.addAssets('Assets/**');
-project.addShaders('Shaders/**');
-project.addSources('src');
-project.addParameter('-main RectPack2Demo'); // sets non standard main so not picked up when using via haxelib
-//project.addParameter('-dce no');
-project.windowOptions.width = 1024;
-project.windowOptions.height = 768;
-resolve( project );
