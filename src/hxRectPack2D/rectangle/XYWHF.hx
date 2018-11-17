8edbd182b04fa4b3ffeb52d86d857273954aefb2
nanjizal <nanjizal@users.noreply.github.com>
2018-11-17 15:07:22 +0000

    adjustments to store image id to allow referencing.

--- src/hxRectPack2D/rectangle/XYWHF.hx
+++ src/hxRectPack2D/rectangle/XYWHF.hx
@@ -1,8 +1,10 @@
 package hxRectPack2D.rectangle;
 class XYWHF extends XYWH {
     public var flipped = false;
-    public function new( x_: Int, y_: Int, w_: Int, h_: Int ){
+    public var id: Int;
+    public function new( id_: Int, x_: Int, y_: Int, w_: Int, h_: Int ){
         super( x_, y_, w_, h_ );
+        id = id_;
     }
     public function flip(){
         flipped = !flipped;
@@ -11,11 +13,17 @@ class XYWHF extends XYWH {
         h = t;
     }
     public function clone(){
-        var res = new XYWHF( x, y, w, h );
+        var res = new XYWHF( id, x, y, w, h );
         res.flipped = flipped;
         return res;
     }
     public static inline function sorts():Array< XYWHF->XYWHF->Int >{
         return Sorts.all();
+    }
+    public function toObject( name: String = '' ){
+        return { id: id, name: name, x: x, y: x, width: w, height: h };
+    }
+    public function string( name: String ){
+        return Std.string( toObject( name ) );
     }
 }
